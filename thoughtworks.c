8cbbfb1b1297575e9f7191a057f839e43686499a
Touhouer <454016110@qq.com>
2019-06-15 15:38:04 +0800

    [LUHANJIANG] add readfile function

--- thoughtworks.c
+++ thoughtworks.c
@@ -1,17 +1,41 @@
 #include <stdio.h>
+#define maxLine 10
+int readfile(int *pArr, int a, int b)
+{
+	int i,j;
+	FILE* fp=fopen("xxx.txt","r"); //打开文件
+	if(fp==NULL)
+	{
+	    printf("无文件");
+	    return 0;
+	}
+	for(i=0;i<a;i++)
+	{
+	    for(j=0;j<b;j++)
+	    {
+	        fscanf(fp, "%d", pArr+b*i+j);//每次读取一个数，fscanf函数遇到空格或者换行结束
+		}
+	    fscanf(fp,"\n");
+	}
+	fclose(fp);
+	
+    return 0;
+}
 
 int main()
 {
-    int i,j,count;
+	int i,j,count;
     char judge;
-    int a[10][10]={0};
-    int b[10][10]={0};
+    int a[maxLine][maxLine]={0};
+    int b[maxLine][maxLine]={0};
 
-    a[1][1]=1;
-    a[1][2]=1;
-    a[1][3]=1;
-    a[2][2]=1;
-    a[3][2]=1;
+    readfile(&a, maxLine, maxLine);
+    
+    /*a[3][3]=1;
+    a[3][4]=1;
+    a[3][5]=1;
+    a[4][4]=1;
+    a[5][4]=1;*/
     
     for (i=0;i<10;i++)
     {
@@ -36,18 +60,18 @@ int main()
         scanf("%c",&judge);
         if (judge=='y')
         {
-            for (i=1;i<9;i++)
+            for (i=0;i<maxLine;i++)//非边界情况 
             {
-                for (j=1;j<9;j++)
+                for (j=0;j<maxLine;j++)
                 {
-                    if (a[i-1][j-1]==1)count++;
-                    if (a[i-1][j]==1)count++;
-                    if (a[i-1][j+1]==1)count++;
-                    if (a[i][j-1]==1)count++;
-                    if (a[i][j+1]==1)count++;
-                    if (a[i+1][j-1]==1)count++;
-                    if (a[i+1][j]==1)count++;
-                    if (a[i+1][j+1]==1)count++;
+                    if ((i-1>=0)&&(j-1>=0)&&a[i-1][j-1]==1)count++;
+                    if ((i-1>=0)&&a[i-1][j]==1)count++;
+                    if ((i-1>=0)&&(j+1<maxLine)&&a[i-1][j+1]==1)count++;
+                    if ((j-1>=0)&&a[i][j-1]==1)count++;
+                    if ((j+1<maxLine)&&a[i][j+1]==1)count++;
+                    if ((i+1<maxLine)&&(j-1>=0)&&a[i+1][j-1]==1)count++;
+                    if ((i+1<maxLine)&&a[i+1][j]==1)count++;
+                    if ((i+1<maxLine)&&(j+1<maxLine)&&a[i+1][j+1]==1)count++;
                 	if (count==2)
                 	{
 						b[i][j]=a[i][j]; 
@@ -63,12 +87,83 @@ int main()
                 	count=0;
                 } 
             }
+            /*
+            if (a[0][1]==1) count++;//左上角的情况 
+            if (a[1][0]==1) count++;
+            if (a[1][1]==1) count++;
+            if (count==2)
+        	{
+				b[0][0]=a[0][0]; 
+        	}
+        	else if (count==3)
+        	{
+            	b[0][0]=1;
+        	}
+        	else
+        	{
+            	b[0][0]=0;
+        	}
+        	count=0;
+        	
+        	if (a[0][maxLine-2]==1) count++;//右上角的情况 
+            if (a[1][maxLine-2]==1) count++;
+            if (a[1][maxLine-1]==1) count++;
+            if (count==2)
+        	{
+				b[0][maxLine-1]=a[0][maxLine-1]; 
+        	}
+        	else if (count==3)
+        	{
+            	b[0][maxLine-1]=1;
+        	}
+        	else
+        	{
+            	b[0][maxLine-1]=0;
+        	}
+        	count=0;
+        	
+        	if (a[maxLine-2][0]==1) count++;//左下角的情况 
+            if (a[maxLine-2][1]==1) count++;
+            if (a[maxLine-1][1]==1) count++;
+            if (count==2)
+        	{
+				b[maxLine-1][0]=a[maxLine-1][0]; 
+        	}
+        	else if (count==3)
+        	{
+            	b[maxLine-1][0]=1;
+        	}
+        	else
+        	{
+            	b[maxLine-1][0]=0;
+        	}
+        	count=0;
+        	
+        	if (a[maxLine-2][maxLine-2]==1) count++;//右下角的情况 
+            if (a[maxLine-2][maxLine-1]==1) count++;
+            if (a[maxLine-1][maxLine-2]==1) count++;
+            if (count==2)
+        	{
+				b[maxLine-1][maxLine-1]=a[maxLine-1][maxLine-1];
+        	}
+        	else if (count==3)
+        	{
+            	b[maxLine-1][maxLine-1]=1;
+        	}
+        	else
+        	{
+            	b[maxLine-1][maxLine-1]=0;
+        	}
+        	count=0;*/
+        	
+            
             for (i=0;i<10;i++)
             {
                 for (j=0;j<10;j++)
                 {
                     a[i][j]=b[i][j];
-                    printf("%d",b[i][j]);
+                    if (b[i][j]==1) printf("*");
+                    else printf("%d",b[i][j]);
                 }
                 printf("\n");
             }
